5297d3466d54b7bc4d9b3864fe7cbbc3bd4eef36
Rafael Sant Anna <rafaeel.16@gmail.com>
2022-07-28 16:01:24 +0100

    Users and Search component finished

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './style.scss';
+
+import logo from '../../assets/logo.png';
 
 export default function Search() {
   const [username, setUsername] = useState('');
 
   return (
     <div className="search-page">
-      <h2>Enter a GitHub username</h2>
+      <img className='logo' src={logo} alt="Github Logo" />
+      <h2 className='search-page__title'>Enter a GitHub username</h2>
       <input className="search-page__input" type="text" onChange={(e) => setUsername(e.target.value)} value={username}/>
       <Link className="search-page__button" to={'/user/' + username}>Search</Link>
   </div>
--- src/Components/User/index.js
+++ src/Components/User/index.js
@@ -1,24 +1,56 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './style.scss';
+import logo from '../../assets/logo.png';
 
 export default function User({match}) {
   const [user, setUser] = useState({});
+  const [respos, setRepos] = useState([]);
   
   useEffect(() => {
     (async () => {
-      const response = await axios.get('https://api.github.com/users/' + match.params.username);
-      setUser(response.data);
-      console.log(response.data);
-    })();
+      let userStorage = localStorage.getItem('users');
+      let repoStorage = localStorage.getItem('repos');
+      let username = localStorage.getItem('username');
 
+      if(userStorage && repoStorage && username === match.params.username) {
+        setUser(JSON.parse(userStorage));
+        setRepos(JSON.parse(repoStorage));
+      } else {
+        const responseUsers = await axios.get('https://api.github.com/users/' + match.params.username);
+        const responseRepos = await axios.get('https://api.github.com/users/' + match.params.username + '/repos');
+        localStorage.setItem('users', JSON.stringify(responseUsers.data));
+        localStorage.setItem('repos', JSON.stringify(responseRepos.data));
+        localStorage.setItem('username', match.params.username);
+        setUser(responseUsers.data);
+        setRepos(responseRepos.data);
+      }
+
+    })();
   }, [match.params.username]);
 
   return (
     <div className="user-page">
-      Profile picture
-      Name
-      total numbers repositories
-      list of repositories (name and description)
+      <Link to='/'>
+        <img className='logo' src={logo} alt="Github Logo" />
+      </Link>
+      
+      <div className='user'>
+        <img className='user__photo' src={user.avatar_url} alt="" />
+        <p className='user__name'>{user.name}</p>
+        <p className='user__total-repositories'>Total repositories: {user.public_repos}</p>
+      </div>
+
+      <div className='repositories'>
+        {respos && respos.map(item => (
+          <div key={item.id} className='repositories__item'>
+            <p className='repositories__name'>{item.name}</p>
+            
+            <p className='repositories__description'>{item.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
